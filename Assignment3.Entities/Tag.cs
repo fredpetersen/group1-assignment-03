89fc3f24420e63437bcb557c46fe9763dabb22c1
Frederik Petersen <fred.p@live.dk>
2022-09-23 14:25:42 +0200

    Tag and User POCO

--- Assignment3.Entities/Tag.cs
+++ Assignment3.Entities/Tag.cs
@@ -2,4 +2,10 @@ namespace Assignment3.Entities;
 
 public class Tag
 {
+    public virtual int id {get; init;}
+
+    [StringLength(50)]
+    public virtual string name{get; init;} //Should be unique, how do we enforce that
+
+    public virtual Task[] tasks{get; set;}
 }
